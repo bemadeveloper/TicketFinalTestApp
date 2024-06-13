3dbb913d1eec735c2588169c1d8eaae9606c5746
bemadeveloper <akbermet1990@icloud.com>
2024-06-13 14:12:24 +0600

    Initial Commit

---
+++ TicketFinalTestApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TicketFinalTestApp
+//
+//  Created by Bema on 13/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TicketFinalTestApp/TicketFinalTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicketFinalTestAppApp.swift
+//  TicketFinalTestApp
+//
+//  Created by Bema on 13/6/24.
+//
+
+import SwiftUI
+
+@main
+struct TicketFinalTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
